50257d4b9e67897e2368686892fb0b0ebe624c4d
sileeee <tldms201@naver.com>
2021-05-30 17:51:26 +0900

    exmaple of ApplicationEventPublisher(Event-based code from Spring 4.2)

--- src/main/java/com/example/demo/MyEvent.java
+++ src/main/java/com/example/demo/MyEvent.java
@@ -1,18 +1,20 @@
 package com.example.demo;
 
-import org.springframework.context.ApplicationEvent;
+public class MyEvent { //4.2부터는 상속받지 않아도 이벤트로 사용 가능
 
-public class MyEvent extends ApplicationEvent { //4.2부터는 상속받지 않아도 이벤트로 사용 가능
-
+    //스프링 프레임워크가 추구하는 철학 - 비침투성(스프링 소스코드가 노출x)
+    //프레임워크의 코드가 노출x -> POJO기반 코드
+    //==> 테스트와 유지보수하기 쉬워진다.
     private int data;
-
-    public MyEvent(Object source){ //빈으로 등록x ,이벤트를 발생시킨 소스만 전송
-        super(source);
-    }
+    private Object source;
 
     public MyEvent(Object source, int data){ //데이터를 실어서 전송 가능
-        super(source);
+        this.source = source;
         this.data = data;
+    }
+
+    public void setSource(int source) {
+        this.source = source;
     }
 
     public int getData(){
--- src/main/java/com/example/demo/MyEventHandler.java
+++ src/main/java/com/example/demo/MyEventHandler.java
@@ -1,16 +1,14 @@
 package com.example.demo;
 
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyEventHandler implements ApplicationListener<MyEvent> { //ApplicationListener<이벤트>구현한 클래스 만들어서 빈으로 등록
+public class MyEventHandler { //ApplicationListener<이벤트>구현한 클래스 만들어서 빈으로 등록
     //4.2부터는 특정 메소드를 구현하지 않아도 됨
 
-    @Override
-    public void onApplicationEvent(MyEvent event){
+    @EventListener
+    public void handler(MyEvent event){
         System.out.println("이벤트 받음. 데이터는 "+event.getData());
     }
-
-
 }
